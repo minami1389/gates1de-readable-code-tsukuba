bcbae217ad8bf271139dc13f88da04b483cf13b1
Yu Kadowaki <gates1de@Yus-MacBook-Pro.local>
2015-07-04 10:56:10 +0900

    Initial Commit

---
+++ readable-code-tsukuba/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  readable-code-tsukuba
+//
+//  Created by Yu Kadowaki on 7/4/15.
+//  Copyright (c) 2015 Yu Kadowaki. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
